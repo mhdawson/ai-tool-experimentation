42b770b590192e481f9a1a7b7b362dce16f4ae1b
Michael Dawson <mdawson@devrus.com>
2024-09-03 18:17:49 -0400

    feat: update flow and functions
    Signed-off-by: Michael Dawson <mdawson@devrus.com>

--- ollama/favorite-color.mjs
+++ ollama/favorite-color.mjs
@@ -32,12 +32,35 @@ const tools =  [{
       required: ['city', 'country'],
     },
   },
-}];
+},
+{
+  type: 'function',
+  function: {
+    name: 'favoriteHockeyTeamTool',
+    description: 'returns the favorite hockey team for person given their City and Country',
+    parameters: {
+      type: 'object',
+      properties: {
+        city: {
+          type: 'string',
+          description: 'the city for the person',
+        },
+        country: {
+          type: 'string',
+          description: 'the country for the person',
+        },
+      },
+      required: ['city', 'country'],
+    },
+  },
+}
+];
 
 /////////////////////////////
 // FUNCTION IMPLEMENTATIONS
 const funcs = {
-  favoriteColorTool: getFavoriteColor
+  favoriteColorTool: getFavoriteColor,
+  favoriteHockeyTeamTool: getFavoriteHockeyTeam
 };
 
 function getFavoriteColor(args) {
@@ -53,12 +76,23 @@ function getFavoriteColor(args) {
   }
 };
 
-
+function getFavoriteHockeyTeam(args) {
+  const city = args.city;
+  const country = args.country;
+  if ((city === 'Ottawa') && (country === 'Canada')) {
+    return 'the favoriteColorTool returned that the favorite hockey team for Ottawa Canada is The Ottawa Senators';
+  } else if ((city === 'Montreal') && (country === 'Canada')) {
+    return 'the favoriteColorTool returned that the favorite hockey team for Montreal Canada is the Montreal Canadians';
+  } else {
+    return `the favoriteColorTool returned The city or country
+            was not valid, please ask the user for them`;
+  }
+};
 
 /////////////////////////////
 // FUNCTION IMPLEMENTATIONS
 // Handle responses which may include a request to run a function
-async function handleResponse(response) {
+async function handleResponse(messages, response) {
   // push the models response to the chat
   messages.push(response.message);
 
@@ -66,6 +100,7 @@ async function handleResponse(response) {
     for (const tool of response.message.tool_calls) {
       // log the function calls so that we see when they are called
       log('  FUNCTION CALLED WITH: ' + inspect(tool));
+      console.log('  CALLED:' + tool.function.name);
       const func = funcs[tool.function.name];
       if (func) {
         const funcResponse = func(tool.function.arguments);
@@ -78,7 +113,7 @@ async function handleResponse(response) {
 
     // call the model again so that it can process the data returned by the 
     // function calls
-    return ( handleResponse(
+    return ( handleResponse( messages,
       await ollama.chat({ model: model, messages: messages, tools: tools})
     ));
   } else { 
@@ -91,17 +126,10 @@ async function handleResponse(response) {
 /////////////////////////////
 // ASK QUESTIONS
 
-// maintains chat history
-const messages = [{type: 'system', content:
-  'only answer questions about a favorite color by using the response from the favoriteColorTool ' +
-  'when asked for a favorite color if you have not called the favoriteColorTool, call it ' + 
-  'Never guess a favorite color ' +
-  'Do not guess the city or country when asked for a favorite color '
-}];
-
 // the model to use
 //const model = 'mistral-nemo';
-const model = 'llama3.1';
+const model = 'mistral';
+//const model = 'llama3.1';
 //const model = 'firefunction-v2';
 
 const questions = ['What is my favorite color?', 
@@ -110,12 +138,27 @@ const questions = ['What is my favorite color?',
                    'I moved to Montreal. What is my favorite color now?',
                    'My city is Montreal and my country is Canada',
                    'My city is Ottawa and my country is Canada, what is my favorite color?',
+                   'What is my favorite hockey team ?',
+                   'My city is Montreal and my country is Canada',
                   ];
 
-for (let i = 0; i< questions.length; i++) {
-  console.log('QUESTION: ' + questions[i]);
-  messages.push({ role: 'user', content: questions[i] });
-  const response = await ollama.chat({ model: model, messages: messages, tools: tools});
-  console.log('  RESPONSE:' + (await handleResponse(response)).message.content);
-}      
+for (let j = 0; j < 10; j++) {
+  // maintains chat history
+  const messages = [{type: 'system', content:
+    'only answer questions about a favorite color by using the response from the favoriteColorTool ' +
+    'when asked for a favorite color if you have not called the favoriteColorTool, call it ' + 
+    'Never guess a favorite color ' +
+    'Do not be chatty ' +
+    'Give short answers when possible'
+  }];
+
+  console.log(`Iteration ${j} ------------------------------------------------------------`);
+
+  for (let i = 0; i< questions.length; i++) {
+    console.log('QUESTION: ' + questions[i]);
+    messages.push({ role: 'user', content: questions[i] });
+    const response = await ollama.chat({ model: model, messages: messages, tools: tools});
+    console.log('  RESPONSE:' + (await handleResponse(messages, response)).message.content);
+  }      
+}
 
