5dd30b5b4502bd12efb5be135cf690c27c3cb55d
Michael Dawson <midawson@redhat.com>
2025-02-04 13:29:49 -0500

    chore: fixup formatting
    Signed-off-by: Michael Dawson <midawson@redhat.com>

--- cross-frameworks/LangGraph/pont-des-arts.mjs
+++ cross-frameworks/LangGraph/pont-des-arts.mjs
@@ -1,14 +1,15 @@
-import { ChatOllama } from "@langchain/ollama";
-import { Agent } from "undici";
-import { MemorySaver } from "@langchain/langgraph";
-import { HumanMessage } from "@langchain/core/messages";
-import { createReactAgent } from "@langchain/langgraph/prebuilt";
-import { DuckDuckGoSearch } from "@langchain/community/tools/duckduckgo_search";
+import { ChatOllama } from '@langchain/ollama';
+import { Agent } from 'undici';
+import { MemorySaver } from '@langchain/langgraph';
+import { HumanMessage } from '@langchain/core/messages';
+import { createReactAgent } from '@langchain/langgraph/prebuilt';
+import { DuckDuckGoSearch } from '@langchain/community/tools/duckduckgo_search';
 
-const OLLAMA_SERVER = "http://10.1.2.38:11434";
+const OLLAMA_SERVER = 'http://10.1.2.38:11434';
 //const MODEL = 'granite3.1-dense';
-const MODEL = "llama3.1";
-//const MODEL = "qwen2.5-coder:32b";
+//const MODEL = 'llama3.1';
+//const MODEL = 'qwen2.5-coder:32b';
+const MODEL = 'llama3.1:8b-instruct-q4_K_M';
 const SHOW_AGENT_PROCESS = false;
 
 const noTimeoutFetch = (input, init) => {
@@ -40,7 +41,7 @@ const agent = createReactAgent({
 async function askQuestion(question) {
   const response = await agent.invoke(
     { messages: [new HumanMessage(question)] },
-    { configurable: { thread_id: "thread1" } },
+    { configurable: { thread_id: 'thread1' } },
   );
   if (SHOW_AGENT_PROCESS) {
     console.log(response.messages);
@@ -50,11 +51,11 @@ async function askQuestion(question) {
 
 // Ask the questions
 const questions = [
-  "How many seconds would it take for a leopard at full speed to run through Pont des Arts?",
-  "How many seconds would it take for a leopard tortoise at full speed to run through Pont des Arts?",
+  'How many seconds would it take for a leopard at full speed to run through Pont des Arts?',
+  'How many seconds would it take for a leopard tortoise at full speed to run through Pont des Arts?',
 ];
 
 for (let i = 0; i < questions.length; i++) {
-  console.log("QUESTION: " + questions[i]);
-  console.log("  RESPONSE:" + (await askQuestion(questions[i])));
+  console.log('QUESTION: ' + questions[i]);
+  console.log('  RESPONSE:' + (await askQuestion(questions[i])));
 }
