d387f1feccc1f3340a3a5b8db1f92337476d7939
Michael Dawson <midawson@redhat.com>
2025-04-02 18:05:24 -0400

    feat: add continuation based RAG example
    Signed-off-by: Michael Dawson <midawson@redhat.com>

--- llama-stack-rag/llama-stack-chat-rag.mjs
+++ llama-stack-rag/llama-stack-chat-rag.mjs
@@ -55,69 +55,51 @@ await client.toolRuntime.ragTool.insert({
   chunk_size_in_tokens: 125,
 });
 
-////////////////////////
-// Create the agent
-
-const agentic_system_create_response = await client.agents.create({
-  agent_config: {
-    model: model_id,
-    instructions:
-      'You are a helpful assistant, answer questions only based on information in the documents provided',
-    toolgroups: [
-      {
-        name: 'builtin::rag/knowledge_search',
-        args: { vector_db_ids: [vector_db_id] },
-      },
-    ],
-    tool_choice: 'auto',
-    input_shields: [],
-    output_shields: [],
-    max_infer_iters: 10,
-  },
-});
-const agent_id = agentic_system_create_response.agent_id;
-
-// Create a session that will be used to ask the agent a sequence of questions
-const sessionCreateResponse = await client.agents.session.create(agent_id, {
-  session_name: 'agent1',
-});
-const session_id = sessionCreateResponse.session_id;
-
 /////////////////////////////
 // ASK QUESTIONS
 
-const questions = ['Should I use npm to start a node.js application'];
+//const questions = ['Should I use npm to start a node.js application'];
+const questions = ['Should I use npm to start an application'];
 
 for (let j = 0; j < 1; j++) {
+  // maintains chat history
+  const messages = [
+    {
+      role: 'system',
+      content: 'Give short answers when possible',
+    },
+  ];
+
   console.log(
     `Iteration ${j} ------------------------------------------------------------`,
   );
 
   for (let i = 0; i < questions.length; i++) {
     console.log('QUESTION: ' + questions[i]);
-    const responseStream = await client.agents.turn.create(
-      agent_id,
-      session_id,
-      {
-        stream: true,
-        messages: [{ role: 'user', content: questions[i] }],
-      },
-    );
 
-    // as of March 2025 only streaming was supported
-    let response = '';
-    for await (const chunk of responseStream) {
-      if (chunk.event.payload.event_type === 'turn_complete') {
-        response = response + chunk.event.payload.turn.output_message.content;
-      } else if (
-        chunk.event.payload.event_type === 'step_complete' &&
-        chunk.event.payload.step_type === 'tool_execution' &&
-        SHOW_RAG_DOCUMENTS
-      ) {
-        console.log(inspect(chunk.event.payload.step_details, { depth: 10 }));
-      }
+    const rawRagResults = await client.toolRuntime.ragTool.query({
+      content: questions[i],
+      vector_db_ids: [vector_db_id],
+    });
+
+    const ragResults = [];
+    for (let j = 0; j < rawRagResults.content.length; j++) {
+      ragResults.push(rawRagResults.content[j].text);
     }
-    console.log('  RESPONSE:' + response);
+
+    const prompt = `Answer the question based only on the context provided
+                   <question>${questions[i]}</question>
+                   <context>${ragResults.join()}</context>`;
+
+    messages.push({
+      role: 'user',
+      content: prompt,
+    });
+    const response = await client.inference.chatCompletion({
+      messages: messages,
+      model_id: model_id,
+    });
+    console.log('  RESPONSE:' + response.completion_message.content);
   }
 }
 
